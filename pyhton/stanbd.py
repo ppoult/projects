9d4d68829ade7816fbc2f2e04b2b72d4ef609ee5
sexy beast <ustasa@otso.city>
2019-06-23 16:37:33 +0300

    NEW BIRTHDAY CHECKER/BASIC FUNCTIONALITY/WORK ON IT!!

--- pyhton/stanbd.py
+++ pyhton/stanbd.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def bdchecker():
+def bdchecka():
     now = datetime.datetime.now()
     stanbd = now.month == 1 and now.day == 5
     return render_template("isitstanbd.html", stanbd=stanbd)
